a29add5360388842747d036154dbb55a2d3385d3
Eltrex <eltrex.eagle@gmail.com>
2020-08-27 18:40:37 +0200

    First new Version
    Trying to do stuff diffrent

--- src/fe/textgame/game.java
+++ src/fe/textgame/game.java
@@ -1,28 +1,43 @@
 package fe.textgame;
 
-import fe.textgame.frame.Window;
+import javax.swing.*;
+import java.awt.Container;
+import java.awt.Color;
 
-public class game implements Runnable{
+public class game
+{
+    //Variables
+    int WIDTH = 800;
+    int HEIGHT = 600;
+    String TITLE = "Text Adventure v0.1";
 
-    //Initial Variables
-    Thread thread;
-    boolean running;
+    JFrame frame;
+    Container container;
+    JPanel TitleTextPanel;
 
-    @Override
-    public void run() {
+    public game()
+    {
+        frame = new JFrame();
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setTitle(TITLE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setBackground(Color.BLACK);
+        frame.setLayout(null);
+        frame.setVisible(true);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
 
+        TitleTextPanel = new JPanel();
+        TitleTextPanel.setBounds(100, 100, 600, 150);
+        TitleTextPanel.setBackground(Color.WHITE);
+
+        container = frame.getContentPane();
+        container.add(TitleTextPanel);
     }
 
-    public synchronized void start(){
-        if(running) return;
-
-        running = true;
-        thread = new Thread(this);
-        thread.start();
+    public static void main(String[] args)
+    {
+        //Starting the Game
+        new game();
     }
-
-    public static void main(String[] args) {
-        new Window(800, 800, "Just a game", new game());
-    }
-
 }
