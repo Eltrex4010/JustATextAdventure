f2cbd252a5fbed193e243be9661fbfa3708db9a0
Eltrex <eltrex.eagle@gmail.com>
2020-08-27 18:50:53 +0200

    Small Update

--- src/fe/textgame/game.java
+++ src/fe/textgame/game.java
@@ -1,8 +1,7 @@
 package fe.textgame;
 
 import javax.swing.*;
-import java.awt.Container;
-import java.awt.Color;
+import java.awt.*;
 
 public class game
 {
@@ -11,9 +10,12 @@ public class game
     int HEIGHT = 600;
     String TITLE = "Text Adventure v0.1";
 
+    //Declaring awt and swing stuff
     JFrame frame;
     Container container;
     JPanel TitleTextPanel;
+    JLabel TitleName;
+    Font TitleFont = new Font("Roboto", Font.PLAIN, 28);
 
     public game()
     {
@@ -30,6 +32,10 @@ public class game
         TitleTextPanel = new JPanel();
         TitleTextPanel.setBounds(100, 100, 600, 150);
         TitleTextPanel.setBackground(Color.WHITE);
+
+        TitleName = new JLabel("ADVENTURE");
+        TitleName.setForeground(Color.BLACK);
+        TitleName.setFont(TitleFont);
 
         container = frame.getContentPane();
         container.add(TitleTextPanel);
